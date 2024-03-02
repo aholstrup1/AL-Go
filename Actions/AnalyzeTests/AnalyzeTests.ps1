e2d384c1c358b9edfb3b94cc5b6d20b09608b0c2
Alexander Holstrup <aholstrup@microsoft.com>
2024-03-02 15:55:37 +0100

    Updates

--- Actions/AnalyzeTests/AnalyzeTests.ps1
+++ Actions/AnalyzeTests/AnalyzeTests.ps1
@@ -15,18 +15,36 @@ try {
     . (Join-Path -Path $PSScriptRoot 'TestResultAnalyzer.ps1')
 
     $testResultFiles = Get-ChildItem -Path $ENV:GITHUB_WORKSPACE -Filter "TestResults.xml" -File -Recurse
+    # Get PR Number from github context
+    $prNumber = $ENV:GITHUB_REF -replace 'refs/pull/(\d+)/merge', '$1'
+    $lastUpdated = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $linkToWorkflowRun = gh api --method GET -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/$ENV:GITHUB_REPOSITORY/actions/runs/$ENV:GITHUB_RUN_ID | ConvertFrom-Json | Select-Object -ExpandProperty html_url
+
     $title = "# Test Results Summary"
-    $summary = $title + "`n`n"
+    $summary = $title + "`n" + "Last updated: $lastUpdated`n" + "Link to workflow run: $linkToWorkflowRun`n`n"
+
     foreach ($testResultFile in $testResultFiles) {
         $testResults = [xml](Get-Content $testResultFile.FullName)
         $testResultSummary = GetTestResultSummary -testResults $testResults -includeFailures 50
 
-        $summary += "### Project / Buildmode (TODO) `n"
+        try {
+            $artifactNamematches = [regex]::Matches($testResultFile.FullName, 'build_projects_(?<project>.+)-(?<branch>.+)-(?<testType>.+)-PR(?<prNumber>\d+)-(?<date>\d+)')
+            if ($artifactNamematches.Success) {
+                $buildMode = $artifactNamematches.Groups['testType'].Value -replace 'TestResults', ''
+                if ($testType -eq '') {
+                    $buildMode = 'Default'
+                }
+            }
+        } catch {
+            $buildMode = 'Unkown'
+            Write-Host $_
+        }
+
+        $summary += "### Project / $buildMode (TODO) `n"
         $summary += "$($testResultSummary.Replace("\n","`n"))`n`n"
-    
     }
     Add-Content -path $ENV:GITHUB_STEP_SUMMARY -value $summary
-    gh api --method POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/$ENV:GITHUB_REPOSITORY/issues/135/comments -f body=$summary
+    gh api --method POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/$ENV:GITHUB_REPOSITORY/issues/$prNumber/comments -f body=$summary
 
     <#$bcptTestResultsFile = Join-Path $ENV:GITHUB_WORKSPACE "$project\BCPTTestResults.json"
     if (Test-Path $bcptTestResultsFile) {
