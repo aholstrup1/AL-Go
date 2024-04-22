91b984878e3b79e4cf8c51bb83faa4987c162e65
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2024-04-22 09:53:00 +0200

    Telemetry Updates (#30)

--- Actions/WorkflowInitialize/WorkflowInitialize.ps1
+++ Actions/WorkflowInitialize/WorkflowInitialize.ps1
@@ -1,14 +1,4 @@
-﻿Param(
-    [Parameter(HelpMessage = "The event id of the initiating workflow", Mandatory = $true)]
-    [string] $eventId
-)
-
-$telemetryScope = $null
-
-try {
-    . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
-    . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-TestRepoHelper.ps1" -Resolve)
-
+﻿function LogAlGoVersion() {
     $ap = "$ENV:GITHUB_ACTION_PATH".Split('\')
     $branch = $ap[$ap.Count-2]
     $owner = $ap[$ap.Count-4]
@@ -22,58 +12,61 @@ try {
     else {
         $verstr = $branch
     }
-
     Write-Big -str "a$verstr"
+}
 
+function LogWorkflowStart() {
+    [System.Collections.Generic.Dictionary[[System.String], [System.String]]] $AdditionalData = @{}
+
+    $alGoSettingsPath = "$ENV:GITHUB_WORKSPACE/.github/AL-Go-Settings.json"
+    if (Test-Path -Path $alGoSettingsPath) {
+        $repoSettings = Get-Content -Path $alGoSettingsPath -Raw -Encoding UTF8 | ConvertFrom-Json
+
+        # Log the repository type
+        if ($repoSettings.PSObject.Properties.Name -contains 'type') {
+            Add-TelemetryData -Hashtable $AdditionalData -Key 'RepoType' -Value $repoSettings.type
+        }
+
+        # Log the template URL
+        if ($repoSettings.PSObject.Properties.Name -contains 'templateUrl') {
+            Add-TelemetryData -Hashtable $AdditionalData -Key 'TemplateUrl' -Value $repoSettings.templateUrl
+        }
+
+        # Log the Al-Go version
+        $alGoVersion = "main"
+        Add-TelemetryData -Hashtable $AdditionalData -Key 'AlGoVersion' -Value $alGoVersion
+    }
+
+    Trace-Information -Message "AL-Go workflow started: $($ENV:GITHUB_WORKFLOW.Trim())" -AdditionalData $AdditionalData
+}
+
+import-module (Join-Path -path $PSScriptRoot -ChildPath "..\TelemetryHelper.psm1" -Resolve)
+
+try {
+
+    . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
+    . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-TestRepoHelper.ps1" -Resolve)
+
+    # Log the start of the workflow to telemetry
+    LogWorkflowStart
+
+    # Log the version of AL-Go that is being used in the workflow
+    LogAlGoVersion
+
+    # Test the AL-Go repository is set up correctly
     TestALGoRepository
 
-    DownloadAndImportBcContainerHelper
-
+    # Test the prerequisites for the test runner
     TestRunnerPrerequisites
 
-    import-module (Join-Path -path $PSScriptRoot -ChildPath "..\TelemetryHelper.psm1" -Resolve)
-    $telemetryScope = CreateScope -eventId $eventId
-    if ($telemetryScope) {
-        $repoSettings = Get-Content -Path (Join-Path $ENV:GITHUB_WORKSPACE '.github/AL-Go-Settings.json') -Raw -Encoding UTF8 | ConvertFrom-Json | ConvertTo-HashTable
-        $type = 'PTE'
-        if ($repoSettings.Keys -contains 'type') {
-            $type = $repoSettings.type
-        }
-        $templateUrl = 'Not set'
-        if ($repoSettings.Keys -contains 'templateUrl') {
-            $templateUrl = $repoSettings.templateUrl
-        }
-        if ($verstr -eq "d") {
-            $verstr = "Developer/Private"
-        }
-        elseif ($verstr -eq "p") {
-            $verstr = "Preview"
-        }
-        AddTelemetryProperty -telemetryScope $telemetryScope -key "ALGoVersion" -value $verstr
-        AddTelemetryProperty -telemetryScope $telemetryScope -key "type" -value $type
-        AddTelemetryProperty -telemetryScope $telemetryScope -key "templateUrl" -value $templateUrl
-        AddTelemetryProperty -telemetryScope $telemetryScope -key "repository" -value $ENV:GITHUB_REPOSITORY
-        AddTelemetryProperty -telemetryScope $telemetryScope -key "runAttempt" -value $ENV:GITHUB_RUN_ATTEMPT
-        AddTelemetryProperty -telemetryScope $telemetryScope -key "runNumber" -value $ENV:GITHUB_RUN_NUMBER
-        AddTelemetryProperty -telemetryScope $telemetryScope -key "runId" -value $ENV:GITHUB_RUN_ID
-
-        $scopeJson = strToHexStr -str ($telemetryScope | ConvertTo-Json -Compress)
-        $correlationId = ($telemetryScope.CorrelationId).ToString()
-    }
-    else {
-        $scopeJson = '7b7d'
-        $correlationId = [guid]::Empty.ToString()
-    }
+    # Create a json object that contains an entry for the workflowstarttime
+    $scopeJson = @{
+        "workflowStartTime" = [DateTime]::UtcNow
+    } | ConvertTo-Json -Compress
 
     Add-Content -Encoding UTF8 -Path $env:GITHUB_OUTPUT -Value "telemetryScopeJson=$scopeJson"
-    Write-Host "telemetryScopeJson=$scopeJson"
-
-    Add-Content -Encoding UTF8 -Path $env:GITHUB_OUTPUT -Value "correlationId=$correlationId"
-    Write-Host "correlationId=$correlationId"
 }
 catch {
-    if (Get-Module BcContainerHelper) {
-        TrackException -telemetryScope $telemetryScope -errorRecord $_
-    }
+    Trace-Exception -ErrorRecord $_
     throw
 }
--- Actions/WorkflowPostProcess/WorkflowPostProcess.ps1
+++ Actions/WorkflowPostProcess/WorkflowPostProcess.ps1
@@ -1,26 +1,36 @@
 ﻿Param(
-    [Parameter(HelpMessage = "The event Id of the initiating workflow", Mandatory = $true)]
-    [string] $eventId,
     [Parameter(HelpMessage = "Telemetry scope generated during the workflow initialization", Mandatory = $false)]
-    [string] $telemetryScopeJson = '7b7d'
+    [string] $telemetryScopeJson = ''
 )
 
-$telemetryScope = $null
-
-try {
-    . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
-    DownloadAndImportBcContainerHelper
-    import-module (Join-Path -path $PSScriptRoot -ChildPath "..\TelemetryHelper.psm1" -Resolve)
-
-    Write-Host "Post-processing workflow $eventId"
-    if ($telemetryScopeJson -and $telemetryScopeJson -ne '7b7d') {
-        $telemetryScope = RegisterTelemetryScope (hexStrToStr -hexStr $telemetryScopeJson)
-        TrackTrace -telemetryScope $telemetryScope
+function LogWorkflowEnd($TelemetryScopeJson) {
+    [System.Collections.Generic.Dictionary[[System.String], [System.String]]] $AdditionalData = @{}
+    $telemetryScope = $null
+    if ($TelemetryScopeJson -ne '') {
+        $telemetryScope = $TelemetryScopeJson | ConvertFrom-Json
     }
-}
-catch {
-    if (Get-Module BcContainerHelper) {
-        TrackException -telemetryScope $telemetryScope -errorRecord $_
+
+    # Calculate the workflow conclusion using the github api
+    $workflowJobs = gh api /repos/$ENV:GITHUB_REPOSITORY/actions/runs/$ENV:GITHUB_RUN_ID/jobs -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" | ConvertFrom-Json
+    if ($null -ne $workflowJobs) {
+        $failedJobs = $workflowJobs.jobs | Where-Object { $_.conclusion -eq "failure" }
+        if ($null -eq $failedJobs) {
+            $workflowConclusion = "Success"
+        } else {
+            $workflowConclusion = "Failure"
+        }
+        Add-TelemetryData -Hashtable $AdditionalData -Key 'WorkflowConclusion' -Value $workflowConclusion
     }
-    throw
+
+    # Calculate the workflow duration using the github api
+    if ($telemetryScope -and ($null -ne $telemetryScope.workflowStartTime)) {
+        Write-Host "Calculating workflow duration..."
+        $workflowTiming= [DateTime]::UtcNow.Subtract([DateTime]::Parse($telemetryScope.workflowStartTime)).TotalSeconds
+        Add-TelemetryData -Hashtable $AdditionalData -Key 'WorkflowDuration' -Value $workflowTiming
+    }
+
+    Trace-Information -Message "AL-Go workflow ran: $($ENV:GITHUB_WORKFLOW.Trim())" -AdditionalData $AdditionalData
 }
+
+import-module (Join-Path -path $PSScriptRoot -ChildPath "..\TelemetryHelper.psm1" -Resolve)
+LogWorkflowEnd -TelemetryScopeJson $telemetryScopeJson
