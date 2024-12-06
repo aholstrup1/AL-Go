deeec915ba39a1a9a0e973da390372e0f77e3796
aholstrup1 <aholstrup@microsoft.com>
2024-12-06 09:58:21 +0100

    Update

--- Actions/AL-Go-Helper.ps1
+++ Actions/AL-Go-Helper.ps1
@@ -1355,8 +1355,8 @@ function CommitFromNewFolder {
     $status = invoke-git -returnValue status --porcelain=v1
     if ($status) {
         $settings = ReadSettings
+        $title = $commitMessage
         if ($settings.commitMessagePrefix) {
-            # Add the prefix followed by a new line to the commit message
             $commitMessage = "$commitMessage [$($settings.commitMessagePrefix)]"
             $body = "$body`n$($settings.commitMessagePrefix)"
         }
@@ -1383,7 +1383,7 @@ function CommitFromNewFolder {
         }
         invoke-git push -u $serverUrl $branch
         try {
-            invoke-gh pr create --fill --head $branch --repo $env:GITHUB_REPOSITORY --base $ENV:GITHUB_REF_NAME --body "$body"
+            invoke-gh pr create --title $title --head $branch --repo $env:GITHUB_REPOSITORY --base $ENV:GITHUB_REF_NAME --body "$body"
         }
         catch {
             OutputError("GitHub actions are not allowed to create Pull Requests (see GitHub Organization or Repository Actions Settings). You can create the PR manually by navigating to $($env:GITHUB_SERVER_URL)/$($env:GITHUB_REPOSITORY)/tree/$branch")
