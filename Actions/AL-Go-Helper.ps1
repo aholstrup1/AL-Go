cdfc813cd4eba8e3ab01db60624ea8987635a7cc
aholstrup1 <aholstrup@microsoft.com>
2024-12-11 14:06:37 +0100

    Cleanup

--- Actions/AL-Go-Helper.ps1
+++ Actions/AL-Go-Helper.ps1
@@ -512,9 +512,7 @@ function ReadSettings {
         [string] $workflowName = "$ENV:GITHUB_WORKFLOW",
         [string] $userName = "$ENV:GITHUB_ACTOR",
         [string] $branchName = "$ENV:GITHUB_REF_NAME",
-        [string] $orgSettingsVariableValue = "$ENV:ALGoOrgSettings",
-        [string] $repoSettingsVariableValue = "$ENV:ALGoRepoSettings",
-        [string] $eventName = "$ENV:GITHUB_EVENT_NAME"
+        [string] $orgSettingsVariableValue = "$ENV:ALGoOrgSettings"
     )
 
     # If the build is triggered by a pull request the refname will be the merge branch. To apply conditional settings we need to use the base branch
