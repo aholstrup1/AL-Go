7c6b1633493a6c9c9d2d5983787c75b5e8a0b537
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2025-01-06 17:03:34 +0100

    Update init.ps1

--- .devcontainer/init.ps1
+++ .devcontainer/init.ps1
@@ -1,7 +1,3 @@
 # Install pre-commit
 pip install pre-commit
 pre-commit install --install-hooks --overwrite
-
-# Install powershell modules
-Install-Module -Name Pester -Force
-Install-Module -Name BCContainerHelper -Force
